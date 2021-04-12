8c1dd1ed5e59fd137b5b776c5b4626e490965c2e
Samuel Larsson <samla949@student.liu.se>
2021-04-12 19:27:45 +0200

    add some ws-stuff

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,56 @@
 import * as express from "express";
 import { Request, Response } from "express";
-
-const app = express();
+import * as WebSocket from 'ws';
+import { v4 as uuidv4 } from 'uuid';
 const { PORT = 3000 } = process.env;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send({
-    message: "Hello world!",
+const app = express();
+const server = app.listen(PORT, () => console.log('started..'));
+const wss = new WebSocket.Server({ server: server });
+
+// data
+const players = new Map<string, WebSocket | null>();
+
+// middlewares
+app.use((req: Request, res: Response, next: Function) => {
+  res.set({
+    'Access-Control-Allow-Credentials': 'true',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Origin, Content-type, Accept',
+    'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, HEAD, OPTIONS'
   });
+  next();
 });
 
-app.listen(PORT, () => {
-  console.log("server started at http://localhost:" + PORT);
+// routes
+app.get('/', (req: Request, res: Response) => {
+  res.json({ numberOfPlayers: players.size, test: 'ok?' });
+});
+
+app.post('/create', (req: Request, res: Response) => {
+  const id = uuidv4();
+  players.set(id, null);
+  res.json({ gameId: 'TODO', token: id });
+});
+
+app.post('/join', (req: Request, res: Response) => {
+  res.json({});
+});
+
+// socket 
+wss.on('connection', (ws: WebSocket) => {
+
+  console.log('open..?')
+  ws.on('open', (_ws: WebSocket) => {
+    console.log('maybe open..?')
+  });
+
+  ws.on('message', (msg: string) => {
+    console.log(msg);
+  });
+
+  ws.on('close', (_ws: WebSocket) => { });
+  ws.on('error', (_ws: WebSocket) => { });
+  ws.on('ping', (_ws: WebSocket) => { });
+  ws.on('pong', (_ws: WebSocket) => { });
 });
